d57dbf2ee7cf8bbf034769111418d7ba5348d1f0
Daria <ladydashaberezina@mail.ru>
2023-07-05 10:14:39 +0000

    Refactor

---
+++ Spin/Spin.cs
@@ -0,0 +1,34 @@
+﻿namespace SpinSpc
+{
+public class Spin
+{
+    private double _angle = 0;
+    private double _angle_speed = 0;
+    private bool _canSpin = true;
+     public void SetAngle(double angle)
+    {
+        _angle = angle;
+    }
+     public void SetAngleSpeed(double angle_speed)
+    {
+        _angle_speed = angle_speed;
+    }
+     public void CanSpin(bool canSpin)
+    {
+        _canSpin = canSpin;
+    }
+    public double SpinAt()
+    {
+        if (double.IsNaN(_angle) || double.IsNaN(_angle_speed) || !_canSpin)
+        {
+            throw new System.Exception();
+        }
+        else
+        {
+            _angle += _angle_speed;
+        }
+        return _angle;
+    }
+}
+
+}
--- spacebattle.Tests/UnitTest1.cs
+++ spacebattle.Tests/UnitTest1.cs
@@ -1,4 +1,3 @@
-using spacebattle;
 using TechTalk.SpecFlow;
 
 namespace spacebattle.Tests;
@@ -7,16 +6,22 @@ namespace spacebattle.Tests;
 public class Движение_космического_корабля
 {
     private readonly ScenarioContext _scenarioContext;
-    private Spaceship _spc = new Spaceship(true);
+    private Spaceship _spc = new Spaceship();
     private double[] _actual = new double[2];
+    private double _actual_angle = 0;
 
     private static void Act() { }
     private static Action _act = () => Act();
 
     public Движение_космического_корабля(ScenarioContext scenarioContext)
     {
+        _spc.fuel.SetFuel(100);
+        _spc.fuel.SetFuelConsumption(1);
+
         _scenarioContext = scenarioContext;
     }
+
+    // Тесты на проверку движения по прямой.
 
     [Given(@"космический корабль находится в точке пространства с координатами \((.*), (.*)\)")]
     public void ДопустимКосмическийКорабльНаходитсяВТочкеПространстваСКоординатами(double p0, double p1)
@@ -73,5 +78,74 @@ public class Движение_космического_корабля
     {
 
         Assert.Throws<System.Exception>(_act);
+    }
+
+    // Тесты для топлива.
+
+    [Given(@"космический корабль имеет топливо в объеме (.*) ед")]
+    public void ДопустимКосмическийКорабльИмеетТопливоВОбъемеЕд(double p0)
+    {
+        _spc.fuel.SetFuel(p0);
+    }
+        
+    [Given(@"имеет скорость расхода топлива при движении (.*) ед")]
+    public void ДопустимИмеетСкоростьРасходаТопливаПриДвиженииЕд(double p0)
+    {
+        _spc.fuel.SetFuelConsumption(p0);
+    }
+        
+    [Then(@"новый объем топлива космического корабля равен (.*) ед")]
+    public void ТоНовыйОбъемТопливаКосмическогоКорабляРавенЕд(double p0)
+    {
+        Assert.Equal(p0, _spc.fuel_volume);
+    }
+
+     // Тесты для вращения.
+
+    [Given(@"космический корабль имеет угол наклона (.*) град к оси OX")]
+    public void ДопустимКосмическийКорабльИмеетУголНаклонаГрадКОсиOX(double p0)
+    {
+        _spc.spin.SetAngle(p0);
+    }
+    [Given(@"космический корабль, угол наклона которого невозможно определить")]
+    public void ДопустимКосмическийКорабльУголНаклонаКоторогоНевозможноОпределить()
+    {
+        _spc.spin.SetAngle(double.NaN);
+    }
+        
+    [Given(@"имеет мгновенную угловую скорость (.*) град")]
+    public void ДопустимИмеетМгновеннуюУгловуюСкоростьГрад(double p0)
+    {
+        _spc.spin.SetAngleSpeed(p0);
+    }
+    [Given(@"мгновенную угловую скорость невозможно определить")]
+    public void ДопустимМгновеннуюУгловуюСкоростьНевозможноОпределить()
+    {
+        _spc.spin.SetAngleSpeed(double.NaN);
+    }
+
+    [Given(@"невозможно изменить уголд наклона к оси OX космического корабля")]
+    public void ДопустимНевозможноИзменитьУголдНаклонаКОсиOXКосмическогоКорабля()
+    {
+        _spc.spin.CanSpin(false);
+    }
+        
+    [When(@"происходит вращение вокруг собственной оси")]
+    public void КогдаПроисходитВращениеВокругСобственнойОси()
+    {
+        try
+        {
+            _actual_angle = _spc.spin.SpinAt();
+        }
+        catch
+        {
+            _act = () => _spc.spin.SpinAt();
+        }
+    }
+        
+    [Then(@"угол наклона космического корабля к оси OX составляет (.*) град")]
+    public void ТоУголНаклонаКосмическогоКорабляКОсиOXСоставляетГрад(int p0)
+    {
+        Assert.Equal(p0, _actual_angle);
     }
 }
